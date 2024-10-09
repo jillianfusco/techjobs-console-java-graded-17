ddc59011f954a0fcdd22ab44c6e7485e303b6b61
Jillian Fusco <fuscojillian@gmail.com>
2024-10-09 19:17:13 -0400

    passing autograder

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -2,13 +2,11 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
